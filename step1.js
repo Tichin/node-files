7d3b2dab3ef84b813f97c49f0475305404f4b2e6
Tichin Wu <twu38@mail.ccsf.edu>
2023-08-08 12:05:12 -0700

    step1 finished

---
+++ step1.js
@@ -0,0 +1,28 @@
+"use strict";
+const fsP = require("fs/promises");
+
+const filepath = process.argv[2];
+
+/** This function takes one argument, filepath,
+ * and it reads the file with that path,
+ * and prints the contents of that file. */
+
+async function cat(filepath) {
+
+  try {
+
+    const content = await fsP.readFile(filepath, 'utf8');
+    console.log(content);
+
+  } catch {
+
+    console.log(`Error reading ${filepath}`);
+    console.log(`Error: ENOENT: no such file or directory, open '${filepath}'`);
+
+    process.exit(1);
+
+  }
+
+}
+
+cat(filepath);
